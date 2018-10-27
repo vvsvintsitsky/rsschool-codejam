968e7a5e2b2fab20148f1fda32bd0a2aa175e438
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-10-27 23:27:32 +0300

    refactor: refactor sumOfOther function to meet airbnb style guide

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,10 +1,10 @@
 const sumOfOther = (inputArray) => {
   const resultArray = [];
   const sumOfArrayElements = inputArray.reduce((accumulator, element) => accumulator + element, 0);
-  for(let i = 0; i < inputArray.length; i++) {
-    resultArray.push(sumOfArrayElements - inputArray[i]); 
+  for (let i = 0; i < inputArray.length; i += 1) {
+    resultArray.push(sumOfArrayElements - inputArray[i]);
   }
   return resultArray;
-}
+};
 
 export default sumOfOther;
