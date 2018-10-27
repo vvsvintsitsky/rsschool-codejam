eb4a93b08b9559c929a2636d6b61cc443c1398a5
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-10-28 00:07:59 +0300

    test: cover recursion function with tests

---
+++ test/recursionTest.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const recursion = require('../src/recursion');
+
+describe('make', () => {
+  describe('sum', () => {
+    it('should return [[100], [90, 120], [70, 99, 110, 130]]', () => {
+      const tree = {
+        value: 100,
+        left: {
+          value: 90,
+          left: {
+            value: 70,
+          },
+          right: {
+            value: 99,
+          },
+        },
+        right: {
+          value: 120,
+          left: {
+            value: 110,
+          },
+          right: {
+            value: 130,
+          },
+        },
+      };
+
+      const result = recursion(tree);
+      assert.deepStrictEqual(result, [[100], [90, 120], [70, 99, 110, 130]]);
+    });
+  });
+});
