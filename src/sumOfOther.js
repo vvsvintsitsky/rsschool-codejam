640dfb00516e4e443cda461486e536287189b487
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-10-27 22:33:55 +0300

    feat: add sumOfOther function

---
+++ src/sumOfOther.js
@@ -0,0 +1,10 @@
+const sumOfOther = (inputArray) => {
+  const resultArray = [];
+  const sumOfArrayElements = inputArray.reduce((accumulator, element) => accumulator + element, 0);
+  for(let i = 0; i < inputArray.length; i++) {
+    resultArray.push(sumOfArrayElements - inputArray[i]); 
+  }
+  return resultArray;
+}
+
+export default sumOfOther;
